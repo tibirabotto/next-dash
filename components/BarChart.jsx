c788d5ef370cc54342071bc534cb6b87b349bff5
Tibira Botto <tibira.botto@gmail.com>
2024-03-31 19:41:38 -0400

    Finished Barchar

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -20,7 +20,41 @@ ChartJS.register(
 );
 
 function BarChart() {
-  return <div>BarChart</div>;
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
+  const [chartOptions, setChartOptions] = useState({});
+  useEffect(() => {
+    setChartData({
+      labels: ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"],
+      datasets: [
+        {
+          label: "Sales $",
+          data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+          borderColor: "rgb(53, 162, 235)",
+          backgroundColor: "rgb(53, 162, 235, 0.4",
+        },
+      ],
+    });
+    setChartOptions({
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "Daily Revenue",
+        },
+      },
+      maintainAspectRatio: false,
+      responsive: true,
+    });
+  }, []);
+  return (
+    <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
+      <Bar data={chartData} options={chartOptions} />
+    </div>
+  );
 }
 
 export default BarChart;
