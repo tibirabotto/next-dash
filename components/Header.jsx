e669e7534974c7a88d932f1fb852cc1ce50fb1d0
Tibira Botto <tibira.botto@gmail.com>
2024-03-31 18:58:38 -0400

    Create Header

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-between px-4 pt-4">
+      <h2>Dasboard</h2>
+      <h2>Wellcome Back, Client</h2>
+    </div>
+  );
+}
+
+export default Header;
